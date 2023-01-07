513744aa0795bb2e5625c99d543ae3372ab58856
muhammadtaufikhidayat <muhtopik07@gmail.com>
2023-01-07 12:33:32 +0700

    add notify detail

--- src/pages/DetailProduct.tsx
+++ src/pages/DetailProduct.tsx
@@ -1,23 +1,40 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { ErorNotify } from '../features/ToastNotify';
 
 const DetailProduct = () => {
   const [productName, setProductName] = useState('');
   const [productPrice, setProductPrice] = useState('');
   const [productDescription, setProductDescription] = useState('');
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
   const getData = async (id: any) => {
-    const { data } = await axios.get(`https://mern-stack-backend-umber.vercel.app/v1/api/product/${id}`);
-    setProductName(data.data.name);
-    setProductPrice(data.data.price);
-    setProductDescription(data.data.description);
+    try {
+      const { data } = await axios.get(`https://mern-stack-backend-umber.vercel.app/v1/api/product/${id}`);
+      setLoading(true);
+      setProductName(data.data.name);
+      setProductPrice(data.data.price);
+      setProductDescription(data.data.description);
+    } catch (error: any) {
+      ErorNotify(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     getData(id);
   }, []);
+
+  if (loading) {
+    return (
+      <>
+        <h1 className="text-center text-white">Loading</h1>
+      </>
+    );
+  }
 
   return (
     <form className="flex justify-center">
@@ -26,19 +43,19 @@ const DetailProduct = () => {
           <label className="label">
             <span className="label-text">Name Product:</span>
           </label>
-          <input placeholder="Type Here" value={productName} className="input input-bordered w-full max-w-md disabled" />
+          <input placeholder="Type Here" value={productName} className="input input-bordered w-full max-w-md " disabled />
         </div>
         <div className="form-control w-full max-w-md">
           <label className="label">
             <span className="label-text">Price Product:</span>
           </label>
-          <input placeholder="Type Here" value={productPrice} className="input input-bordered w-full max-w-md disabled" />
+          <input placeholder="Type Here" value={productPrice} className="input input-bordered w-full max-w-md " disabled />
         </div>
         <div className="form-control w-full max-w-md">
           <label className="label">
             <span className="label-text">Description:</span>
           </label>
-          <input placeholder="Type Here" value={productDescription} className="input input-bordered w-full max-w-md disabled" />
+          <input placeholder="Type Here" value={productDescription} className="input input-bordered w-full max-w-md " disabled />
         </div>
       </div>
     </form>
